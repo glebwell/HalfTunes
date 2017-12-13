534538b8d99ea60cc2607f4a9a5212f9e1265ab8
glebwell <gleb.maltcev@yandex.ru>
2017-12-13 19:25:26 +0300

    first unit test was added

---
+++ HalfTunesSlowTests/HalfTunesSlowTests.swift
@@ -0,0 +1,51 @@
+//
+//  HalfTunesSlowTests.swift
+//  HalfTunesSlowTests
+//
+//  Created by Gleb on 13.12.17.
+//  Copyright © 2017 Ray Wenderlich. All rights reserved.
+//
+
+import XCTest
+@testable import HalfTunes
+
+class HalfTunesSlowTests: XCTestCase {
+
+    var sessionUnderTest: URLSession!
+    
+    override func setUp() {
+        super.setUp()
+        sessionUnderTest = URLSession(configuration: URLSessionConfiguration.default)
+    }
+    
+    override func tearDown() {
+        sessionUnderTest = nil
+        super.tearDown()
+    }
+    
+    func testValidCallToiTunesGetsHTTPStatusCode200() {
+        //given
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+
+        let promise = expectation(description: "Status code: 200")
+
+        //when
+        let dataTask = sessionUnderTest.dataTask(with: url!) { data, response, error in
+            // then
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
+
+    }
+        dataTask.resume()
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+}
