e68cf1f2e3bec854f51b8a1961e9be8f67175782
glebwell <gleb.maltcev@yandex.ru>
2017-12-14 23:05:41 +0300

    implemented until Fake update to mock object

--- HalfTunesSlowTests/HalfTunesSlowTests.swift
+++ HalfTunesSlowTests/HalfTunesSlowTests.swift
@@ -47,5 +47,30 @@ class HalfTunesSlowTests: XCTestCase {
         dataTask.resume()
         waitForExpectations(timeout: 5, handler: nil)
     }
+
+    func testCallToiTunesCompletes() {
+        // given
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+
+        // when
+        let dataTask = sessionUnderTest.dataTask(with: url!) { data, response, error in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+
+            promise.fulfill()
+        }
+        dataTask.resume()
+
+        waitForExpectations(timeout: 5, handler: nil)
+
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
+
+    }
     
 }
